2594c4a7b534b483443cd815464224659c6ce95d
chshivm007 <chshivm007@gmail.com>
2023-01-30 19:24:02 +0530

    HashMap Code

--- CP/code.java
+++ CP/code.java
@@ -3,13 +3,79 @@ import java.io.*;
 
 public class code {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test = Integer.parseInt(br.readLine());
-        while (test-- > 0) {
-            int num = Integer.parseInt(br.readLine());
-            System.out.println(num);
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+  
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
         }
+  
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+  
+        int nextInt() { return Integer.parseInt(next()); }
+  
+        long nextLong() { return Long.parseLong(next()); }
+  
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+  
+        String nextLine()
+        {
+            String str = "";
+            try {
+                if(st.hasMoreTokens()){
+                    str = st.nextToken("\n");
+                }
+                else{
+                    str = br.readLine();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static boolean isPrime(long num) {
+        if (num == 2)
+            return true;
+        if  (num == 1)
+            return false;
+        for (int i = 2; i <= (int)Math.sqrt(num); ++i) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        FastReader s = new FastReader();
+        String str = s.next();
+        int upper = 0;
+
+        for (int i = 0; i < str.length(); ++i) {
+            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z')
+                ++upper;
+        }
+        if (str.length() / 2 < upper)
+            System.out.println(str.toUpperCase());
+        else System.out.println(str.toLowerCase());
     }
 }
 // int[] input = Arrays.stream(br.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
--- IdeaProjects/Competitive/Hashmap.java
+++ IdeaProjects/Competitive/Hashmap.java
@@ -1,12 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Hashmap {
     public static void main (String[] args) throws IOException {
-        Bufferd
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        List<Character> singles = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < input.length(); ++i) {
+            if (map.containsKey(input.charAt(i)))
+                map.put(input.charAt(i), map.get(input.charAt(i)) + 1);
+            else map.put(input.charAt(i), 1);
+        }
+        for (Character key : map.keySet()) {
+            if (map.get(key) == 1)
+                singles.add(key);
+        }
+
+        for (int i = 0; i < input.length(); ++i) {
+            if (singles.contains(input.charAt(i))) {
+                System.out.println(input.charAt(i));
+                break;
+            }
+        }
 
 
     }
